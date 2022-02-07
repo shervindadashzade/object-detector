255b4ef8dc8eb376852a0ecd6521a11157231366
shervindadashzade <shervin.dadashzade7988@gmail.com>
2022-02-07 05:29:35 +0330

    second phase

--- server.py
+++ server.py
@@ -4,11 +4,18 @@ import os
 import random
 import string
 from yolov5.detect import run
+from flask_cors import CORS
+import urllib
+import validators
 
 app = Flask(__name__)
+cors = CORS(app)
 
 app.config['UPLOAD_FOLDER'] = os.path.join('static','uploads')
 app.config['RESULT_FOLDER'] = os.path.join('static','results')
+
+extensions_list= ['JPG','jpg','png','PNG']
+
 @app.route("/")
 
 def hello_world():
@@ -26,6 +33,8 @@ def uploader_file():
         random_string = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(10))
         extension = f.filename.split('.')[-1]
         print(extension)
+        if extension not in extensions_list:
+            return 'Invalid file extension.'
         file_name = random_string +'.'+ extension
         print(file_name)
         uploaded_path_file = os.path.join(app.config['UPLOAD_FOLDER'],file_name)
@@ -33,6 +42,46 @@ def uploader_file():
         run(weights='best.pt',project='static/results',name=random_string,source=uploaded_path_file)
         return render_template('result.html', result_img=os.path.join(app.config['RESULT_FOLDER'],random_string,file_name))
         return 'saved'
+
+
+@app.post('/api/detect')
+def detect_file():
+        if 'file' not in request.files:
+            return 'No file part.'
+        f = request.files['file']
+        random_string = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(10))
+        extension = f.filename.split('.')[-1]
+        print(extension)
+        if extension not in extensions_list:
+            return 'Invalid Image.'
+        file_name = random_string +'.'+ extension
+        print(file_name)
+        uploaded_path_file = os.path.join(app.config['UPLOAD_FOLDER'],file_name)
+        f.save(uploaded_path_file)
+        run(weights='yolov5s.pt',project='static/results',name=random_string,source=uploaded_path_file)
+        result_url = 'http://192.99.230.250:5000/' + os.path.join(app.config['RESULT_FOLDER'],random_string,file_name)
+        return result_url
+
+@app.put('/api/detect')
+def detect_url():
+        if 'url' not in request.form:
+            return 'Missing url field.'
+        url = request.form['url']
+        random_string = ''.join(random.choice(string.ascii_letters + string.digits) for i in range(10))
+        print(random_string)
+        if not validators.url(url):
+            return 'Invalid url.'
+        extension = url.split('.')[-1]
+        file_name = random_string+'.'+extension
+        if extension not in extensions_list:
+            return 'Invalid Image.'
+        opener = urllib.request.build_opener()
+        opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+        urllib.request.install_opener(opener)
+        urllib.request.urlretrieve(url,'static/uploads/'+file_name)
+        run(weights='yolov5s.pt',project='static/results',name=random_string,source='static/uploads/'+file_name)
+        return 'http://192.99.230.250:5000/' + os.path.join(app.config['RESULT_FOLDER'],random_string,file_name)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
